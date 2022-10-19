86abf0fe9aaa0717449309006b32624499a17ede
hamomgs <hamomgomer947@gmail.com>
2022-10-19 00:16:14 -0300

    ✨feat: Languages

---
+++ src/i18n/index.js
@@ -0,0 +1,17 @@
+import i18n from 'i18next'
+import { initReactI18next } from 'react-i18next'
+import enJson from './translations/en.json'
+import ptBRJson from './translations/ptBR.json'
+
+i18n.use(initReactI18next).init({
+  fallbackLng: 'en',
+  interpolation: {
+    escapeValue: false
+  },
+  resources: {
+    en: enJson,
+    ptBR: ptBRJson
+  }
+})
+
+export default i18n
